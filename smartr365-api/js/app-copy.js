d283392dacf8cb59f91e540688fc7469824505f5
wearenv-digital <design@wearenv.co.uk>
2022-08-15 12:19:04 +0100

    take the headers off

--- smartr365-api/js/app-copy.js
+++ smartr365-api/js/app-copy.js
@@ -200,10 +200,10 @@ async function makePost() {
 					"content-type": "application/json",
 					"x-api-key": "2528e9b2-7250-48fc-9371-4c13cd5991a4",
 					"accept": "text/plain",
-					"Access-Control-Allow-Headers": "*",
-					"Access-Control-Allow-Methods": "POST",
-					"Access-Control-Allow-Methods": "OPTIONS",
-					"Access-Control-Allow-Origin": "*",
+					// "Access-Control-Allow-Headers": "*",
+					// "Access-Control-Allow-Methods": "POST",
+					// "Access-Control-Allow-Methods": "OPTIONS",
+					// "Access-Control-Allow-Origin": "*",
 				},
 			}
 		);
