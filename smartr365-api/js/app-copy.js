00ec395d2d23d8f41c106ec2cdd857c0492b6a66
wearenv-digital <design@wearenv.co.uk>
2022-08-15 12:08:17 +0100

    possible error with prettier

--- smartr365-api/js/app-copy.js
+++ smartr365-api/js/app-copy.js
@@ -1,11 +1,11 @@
-const firstNameEl = document.querySelector('#firstName');
-const lastNameEl = document.querySelector('#lastName');
-const emailEl = document.querySelector('#email');
-const telephoneEl = document.querySelector('#telephone');
+const firstNameEl = document.querySelector("#firstName");
+const lastNameEl = document.querySelector("#lastName");
+const emailEl = document.querySelector("#email");
+const telephoneEl = document.querySelector("#telephone");
 // const checkBox = document.getElementById("accept");
 // const submitBtn = document.getElementById("submitButton");
 
-const form = document.querySelector('#newLead');
+const form = document.querySelector("#newLead");
 
 const checkFirstName = () => {
 	let valid = false;
@@ -13,9 +13,9 @@ const checkFirstName = () => {
 	const firstName = firstNameEl.value.trim();
 
 	if (!isRequired(firstName)) {
-		showError(firstNameEl, 'First name cannot be left blank');
+		showError(firstNameEl, "First name cannot be left blank");
 	} else if (!isFirstNameValid(firstName)) {
-		showError(firstNameEl, 'First name must only contain letters');
+		showError(firstNameEl, "First name must only contain letters");
 	} else {
 		showSuccess(firstNameEl);
 		valid = true;
@@ -29,9 +29,9 @@ const checkLastName = () => {
 	const lastName = lastNameEl.value.trim();
 
 	if (!isRequired(lastName)) {
-		showError(lastNameEl, 'Last name cannot be left blank');
+		showError(lastNameEl, "Last name cannot be left blank");
 	} else if (!isLastNameValid(lastName)) {
-		showError(lastNameEl, 'Last name must only contain letters');
+		showError(lastNameEl, "Last name must only contain letters");
 	} else {
 		showSuccess(lastNameEl);
 		valid = true;
@@ -43,9 +43,9 @@ const checkEmail = () => {
 	let valid = false;
 	const email = emailEl.value.trim();
 	if (!isRequired(email)) {
-		showError(emailEl, 'Email field cannot be left blank');
+		showError(emailEl, "Email field cannot be left blank");
 	} else if (!isEmailValid(email)) {
-		showError(emailEl, 'Email is not valid');
+		showError(emailEl, "Email is not valid");
 	} else {
 		showSuccess(emailEl);
 		valid = true;
@@ -56,9 +56,9 @@ const checkTelephone = () => {
 	let valid = false;
 	const telephone = telephoneEl.value.trim();
 	if (!isRequired(telephone)) {
-		showError(telephoneEl, 'Telephone field cannot be left blank');
+		showError(telephoneEl, "Telephone field cannot be left blank");
 	} else if (!isTelephoneValid(telephone)) {
-		showError(telephoneEl, 'Number is not valid');
+		showError(telephoneEl, "Number is not valid");
 	} else {
 		showSuccess(telephoneEl);
 		valid = true;
@@ -87,50 +87,50 @@ const isTelephoneValid = (telephone) => {
 	return re.test(telephone);
 };
 
-const isRequired = (value) => (value === '' ? false : true);
+const isRequired = (value) => (value === "" ? false : true);
 
 const showError = (input, message) => {
 	const formField = input.parentElement;
-	formField.classList.remove('success');
-	formField.classList.add('error');
+	formField.classList.remove("success");
+	formField.classList.add("error");
 
-	const error = formField.querySelector('small');
+	const error = formField.querySelector("small");
 	error.textContent = message;
 };
 
 const showSuccess = (input) => {
 	const formField = input.parentElement;
 
-	formField.classList.remove('error');
-	formField.classList.add('success');
+	formField.classList.remove("error");
+	formField.classList.add("success");
 
-	const error = formField.querySelector('small');
-	error.textContent = '';
+	const error = formField.querySelector("small");
+	error.textContent = "";
 };
 
 //final validation for the form
-const submitBtn = document.getElementById('submitButton');
-let hiddenBox = (document.getElementById('terms').style.visibility = 'hidden');
-let evalResult = '';
+const submitBtn = document.getElementById("submitButton");
+let hiddenBox = (document.getElementById("terms").style.visibility = "hidden");
+let evalResult = "";
 
 //show or hide the terms and conditions check box
 
 function showBox() {
-	document.getElementById('terms').style.visibility = 'visible';
-	console.log('a wild box appeared!');
+	document.getElementById("terms").style.visibility = "visible";
+	console.log("a wild box appeared!");
 }
 
 function hideBox() {
-	document.getElementById('terms').style.visibility = 'hidden';
-	console.log('the box fled!');
+	document.getElementById("terms").style.visibility = "hidden";
+	console.log("the box fled!");
 }
 
 // validation runs on every input and only shows the checkbox
 // when the form is completely vaslidated
 
-let valid = '';
+let valid = "";
 
-document.querySelector('form').addEventListener('input', function () {
+document.querySelector("form").addEventListener("input", function () {
 	let valid = false;
 	if (
 		(checkFirstName(), checkLastName(), checkEmail(), checkTelephone() === true)
@@ -147,10 +147,10 @@ document.querySelector('form').addEventListener('input', function () {
 	}
 });
 
-const cb = document.querySelector('#accept');
-console.log('it is' + cb.checked);
+const cb = document.querySelector("#accept");
+console.log("it is" + cb.checked);
 
-cb.addEventListener('click', function () {
+cb.addEventListener("click", function () {
 	if (cb.checked === true) {
 		submitBtn.disabled = false;
 	} else {
@@ -159,55 +159,55 @@ cb.addEventListener('click', function () {
 });
 
 // post the data
-const wrapper = document.getElementById('wrapper');
-const formWrap = document.getElementById('formWrap');
-const successMsg = document.getElementById('successMsg');
-const applicantH1 = document.getElementById('applicantH1');
-successMsg.style.display = 'none';
+const wrapper = document.getElementById("wrapper");
+const formWrap = document.getElementById("formWrap");
+const successMsg = document.getElementById("successMsg");
+const applicantH1 = document.getElementById("applicantH1");
+successMsg.style.display = "none";
 
 function successTime() {
-	successMsg.style.display = 'flex';
-	applicantH1.style.display = 'none';
-	successMsg.style.flexDirection = 'column';
-	formWrap.style.display = 'none';
+	successMsg.style.display = "flex";
+	applicantH1.style.display = "none";
+	successMsg.style.flexDirection = "column";
+	formWrap.style.display = "none";
 }
 
 //new post function
 
 const corsHeaders = {
-	'Access-Control-Allow-Headers': '*',
-	'Access-Control-Allow-Methods': 'POST',
-	'Access-Control-Allow-Methods': 'OPTIONS',
-	'Access-Control-Allow-Origin': '*',
+	"Access-Control-Allow-Headers": "*",
+	"Access-Control-Allow-Methods": "POST",
+	"Access-Control-Allow-Methods": "OPTIONS",
+	"Access-Control-Allow-Origin": "*",
 };
 
 //makepost function
 async function makePost() {
-	if (Request.method === 'OPTIONS') {
-		return new Response('OK', { headers: corsHeaders });
+	if (Request.method === "OPTIONS") {
+		return new Response("OK", { headers: corsHeaders });
 	}
-	if (Request.method === 'POST') {
+	if (Request.method === "POST") {
 		const formData = new FormData(form);
 		try {
 			const Response = await fetch(
-				'https://api.smartr365.com/api/v1/mortgage/lead/create',
+				"https://api.smartr365.com/api/v1/mortgage/lead/create",
 				{
-					method: 'POST',
+					method: "POST",
 					body: formData,
-					mode: 'cors',
+					mode: "cors",
 					//credentials: 'include',
 					headers: {
-						'content-type': 'application/json',
-						'x-api-key': '2528e9b2-7250-48fc-9371-4c13cd5991a4',
-						accept: 'text/plain',
-						'Access-Control-Allow-Headers': '*',
-						'Access-Control-Allow-Methods': 'POST',
-						'Access-Control-Allow-Methods': 'OPTIONS',
-						'Access-Control-Allow-Origin': '*',
+						"content-type": "application/json",
+						"x-api-key": "2528e9b2-7250-48fc-9371-4c13cd5991a4",
+						"accept": "text/plain",
+						"Access-Control-Allow-Headers": "*",
+						"Access-Control-Allow-Methods": "POST",
+						"Access-Control-Allow-Methods": "OPTIONS",
+						"Access-Control-Allow-Origin": "*",
 					},
 				}
 			);
-			console.log('status code: ', Response.status);
+			console.log("status code: ", Response.status);
 			if (!Response.ok) {
 				console.log(Response);
 				throw new Error(`Error! status; ${Response.status}`);
@@ -222,7 +222,7 @@ async function makePost() {
 	}
 }
 
-form.addEventListener('submit', function (e) {
+form.addEventListener("submit", function (e) {
 	e.preventDefault();
 	makePost();
 });
@@ -243,19 +243,19 @@ function debounce(fn, delay = 500) {
 
 //event delegation
 form.addEventListener(
-	'input',
+	"input",
 	debounce(function (e) {
 		switch (e.target.id) {
-			case 'firstName':
+			case "firstName":
 				checkFirstName();
 				break;
-			case 'lastName':
+			case "lastName":
 				checkLastName();
 				break;
-			case 'email':
+			case "email":
 				checkEmail();
 				break;
-			case 'telephone':
+			case "telephone":
 				checkTelephone();
 				break;
 		}
