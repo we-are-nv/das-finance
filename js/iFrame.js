6dd095f9dc5eb3b9bcd37c3fa0af567098f19ee3
yenmangu <robert.shelford@googlemail.com>
2023-02-20 13:46:45 +0000

    iframe inserted into contact page

--- js/iFrame.js
+++
@@ -1,7 +0,0 @@
-function resizeIframe(iFrame) {
-	iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
-}
-window.addEventListener('DOMContentLoaded', function (e) {
-	let iFrame = document.getElementById('iframe-1');
-	resizeIframe(iFrame);
-});
